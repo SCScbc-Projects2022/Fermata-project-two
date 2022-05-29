7659e1e1708b9eb252ebf3a7315e9f529d1f32bb
Kyle <kylejmbaylis@gmail.com>
2022-05-29 19:43:33 -0400

    all api routes added

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const draftRoutes = require('./draft-routes');
-const historyRoutes = require('./history-routes');
+const sentRoutes = require('./sent-routes');
 const fontRotues = require('./font-routes');
 
 router.use('/users', userRoutes);
 router.use('/drafts', draftRoutes);
-router.use('/history', historyRoutes);
+router.use('/sent', sentRoutes);
 router.use('/fonts', fontRotues);
 
 module.exports = router;
