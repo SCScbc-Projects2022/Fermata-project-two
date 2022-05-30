a899f28be3c6b3a988923b9b7d3cdc111a802e55
Kyle <kylejmbaylis@gmail.com>
2022-05-30 13:50:02 -0400

    cleaning up files

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,4 +1,4 @@
 const router = require('express').Router();
-
+const {Draft, Sent} = require('../models');
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/');
-const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
 
-router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
