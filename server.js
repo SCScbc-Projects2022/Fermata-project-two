a1fc7f5163435983be63108259844300c7d81e54
Kyle <kylejmbaylis@gmail.com>
2022-05-30 14:01:34 -0400

    fixing some error

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-<<<<<<< HEAD
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-=======
 //HANDLEBARS
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -19,9 +16,6 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 //
 
-
-app.use(require('./controllers'));
->>>>>>> develop
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
