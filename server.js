fb34ece1ff71dd1f4e02b36a3d7929c3bc65a46f
LEWIS <admin@admins-MacBook-Pro.local>
2022-05-31 19:26:24 -0400

    updated layouts

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+app.use(session(sess));
 
 app.use(require('./controllers'));
 app.use(express.json());
@@ -11,6 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers'));
 
-sequelize.sync({ force: false }).then(() => {
+ sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
