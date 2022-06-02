99814abc3c99dc45e57becfa5805d4ab819081fc
TOVTC <veronica.tc.to@outlook.com>
2022-06-01 20:34:45 -0400

    updated seed information with uniqid and spotify id

--- seeds/draft-seeds.js
+++ seeds/draft-seeds.js
@@ -2,7 +2,7 @@ const {Draft} = require('../models');
 
 const draftData = [
     {
-        id: 'a1b2c3',
+        id: '1tsl810l3wa2sf9',
         sign_off: 'user1',
         user_id: 1,
         recipient_name: 'recipient1',
@@ -12,33 +12,33 @@ const draftData = [
         font_id: 2
     },
     {
-        id: '3c2b1a',
+        id: '1tslfrsl3wa3y5x',
         sign_off: 'user2',
         user_id: 2,
         recipient_name: null,
         recipient_email: null,
-        letter_body: 'This is a letter from user2', 
-        spotify_id: 'Kdawp3iL',
+        letter_body: 'This is a draft from user2', 
+        spotify_id: '4lCv7b86sLynZbXhfScfm2', // Firework, Katy Perry
         font_id: 2
     },
     {
-        id: 'd4e5f6',
+        id: '31tslfn8l3wa1ltr',
         sign_off: null,
         user_id: 1,
         recipient_name: 'recipient3',
         recipient_email: 'recipient3@email.com',
-        letter_body: 'This is a letter from user1', 
-        spotify_id: '16BvOm77',
+        letter_body: 'This is a draft from user1', 
+        spotify_id: '5TbzAWWc5eJaANpA9kfGCd', // Love Me Again, John Newman
         font_id: 3
     },
     {
-        id: '6f5e4d',
+        id: '1tsl30sl3wa0un8',
         sign_off: 'user3',
         user_id: 3,
         recipient_name: 'recipient4',
         recipient_email: 'recipient4@email.com',
-        letter_body: 'This is a letter from user3', 
-        spotify_id: '7ERXlz6E',
+        letter_body: 'This is a draft from user3', 
+        spotify_id: '0KWcRXcnGoB0gWSu4mshF8', // Electric Blue, Arcade Fire
         font_id: 2
     }
 ]
@@ -46,3 +46,7 @@ const draftData = [
 const seedDrafts = () => Draft.bulkCreate(draftData);
 
 module.exports = seedDrafts;
+
+// Firework, Katy Perry - 4lCv7b86sLynZbXhfScfm2
+// Love Me Again, John Newman - 5TbzAWWc5eJaANpA9kfGCd
+// Electric Blue, Arcade Fire - 0KWcRXcnGoB0gWSu4mshF8
