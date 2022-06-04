55ebe00b9117f261a621f47c2c5ed36ea20df5d0
TOVTC <veronica.tc.to@outlook.com>
2022-06-04 00:20:43 -0400

    I have no idea why I can't even GET the homepage

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,16 +3,15 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const draftRoutes = require('./draft-routes');
 const sentRoutes = require('./sent-routes');
-const fontRotues = require('./font-routes');
+const fontRoutes = require('./font-routes');
 
 router.use('/users', userRoutes);
 router.use('/drafts', draftRoutes);
 router.use('/sent', sentRoutes);
-router.use('/fonts', fontRotues);
+router.use('/fonts', fontRoutes);
 
 router.get('/', (req, res) => {
-    res.render('homepage');
-  });
-  
+  res.render('homepage');
+});
 
 module.exports = router;
