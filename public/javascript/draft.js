94af7e24f09081fa372a0fc5be48fd8842e3dc24
TOVTC <veronica.tc.to@outlook.com>
2022-06-04 17:06:34 -0400

    comitting to complete code review

--- public/javascript/draft.js
+++ public/javascript/draft.js
@@ -25,53 +25,51 @@ if (window.location.pathname === '/sent') {
     heading.style.display = 'none';
 }
 
-// add an if statment to conditionally declare variables for the page when passed as a parameter from backend
+// add an if statment to conditionally declare variables for the page when passed as a parameter from backend -- probably no time to do this
 
 // send letter
 sendBtn = document.querySelector('#send-btn').addEventListener('click', async () => {
-//     // let id = uniqid(); npm packages are backend only add to API POST requests
-//     // res.render('/pathname, {key: value} - to pass variables from backend to frontend) + let x = {{key}} in js
-//     sign_off = document.querySelector();
-//     recipient_name = document.querySelector();
-//     recipient_email = document.querySelector();
-//     spotify_id = 'a1b2c3' // ----- placeholder value
-//     font_id = document.querySelector(); //ability not yet available
-//     letter_body = document.querySelector();
-//     if (!sign_off.value || !recipient_email.value || !spotify_id.value || !font_id.value || !letter_body.value) {
-//         alert('Please ensure all fields are completed before sending your message')
-//     }
-//     if (letter_body.value > 255) {
-//         alert('Your message should be less than 255 characters');
-//     }
-//     let format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(recipient_email.value);
-//     if (!format) {
-//         alert('Please enter a valid email');
-//         return
-//     }
-//     const sendLetter = await fetch('api/sent', {
-//         method: 'POST',
-//         body: JSON.stringify({
-//             sign_off,
-//             recipient_name,
-//             recipient_email,
-//             spotify_id,
-//             font_id,
-//             letter_body
-//         }),
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     });
-//     if (sendLetter.ok) {
-//         console.log('success!');
+    // let id = uniqid(); npm packages are backend only add to API POST requests
+    // res.render('/pathname, {key: value} - to pass variables from backend to frontend) + let x = {{key}} in js
+    sign_off = document.querySelector();
+    recipient_name = document.querySelector();
+    recipient_email = document.querySelector();
+    spotify_id = 'a1b2c3' // ----- placeholder value
+    font_id = document.querySelector(); //ability not yet available
+    letter_body = document.querySelector();
+    if (!sign_off.value || !recipient_email.value || !spotify_id.value || !font_id.value || !letter_body.value) {
+        alert('Please ensure all fields are completed before sending your message')
+    }
+    if (letter_body.value > 255) {
+        alert('Your message should be less than 255 characters');
+    }
+    let format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(recipient_email.value);
+    if (!format) {
+        alert('Please enter a valid email');
+        return
+    }
+    const sendLetter = await fetch('api/sent', {
+        method: 'POST',
+        body: JSON.stringify({
+            sign_off,
+            recipient_name,
+            recipient_email,
+            spotify_id,
+            font_id,
+            letter_body
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (sendLetter.ok) {
         var email = document.createElement("a");
         email.href = `mailto:${address}?subject=\u{1F4C3} Your Fermata Has Arrived \u{1F3B5}&body=Hey ${recipient}, %0D%0A %0D%0A${sender} just sent you a Fermata - a personal letter with a song chosen just for you! %0D%0AExperience it here: ${link}%0D%0A %0D%0AWith \u{1F498}, %0D%0AThe Fermata Team %0D%0A %0D%0A `;
         email.click();
-//      //document.location.replace('./Dashboard.html');
-//     } else {
-//         alert(response.statusText);
-//     }
-//     //document.location.replace('./Sent.html');
+        document.location.replace('./confirm');
+    } else {
+        alert(response.statusText);
+    }
 });
 
 // YOU -> THEM -> SONG -> STYLE -> SCRIPT(SEND) -> PREVIEW -> SENT -> DASHBOARD
