c6c1576d023b07452ec7f54929690b8b1e731b9a
Schris <tovechris48@gmail.com>
2022-06-05 01:08:10 -0400

    callback-index-api-response-handler

--- spotify/callback/index.php
+++ spotify/callback/index.php
@@ -2,7 +2,7 @@
 
 // Spotify API response handler
 
-if (!empty($_GET['code'])) include '_inc/requestLogin.php';
+if (!empty($_GET['code'])) include '../requestLogin.php';
 else {
   echo '<pre>';
   print_r($_GET);
