6a586c0cc1d33fc3ffa485d10b4f79eb8979bd7c
TOVTC <veronica.tc.to@outlook.com>
2022-06-05 19:20:25 -0400

    had to delete partials :( add delete button functionality in future

---
+++ public/javascript/dashboard.js
@@ -0,0 +1,5 @@
+document.querySelector('.delete-btn').addEventListener('click', (event) => {
+    console.log(event.Target);
+})
+
+// the logic here doesn't work yet so this is a project for another day
